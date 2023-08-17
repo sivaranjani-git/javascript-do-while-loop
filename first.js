ef873f36037ed771321ac077b41212243ca795fb
sivaranjani-git <134845313+sivaranjani-git@users.noreply.github.com>
2023-08-17 11:00:34 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,32 @@
+//print sum of positive numbers
+
+
+//var number=0;
+//while(number>=0){
+//sum=number;
+
+//number=parseInt(prompt("enter any number")); 
+//if(isNaN(number)){
+//alert("you have entered a string.please enter a number");
+//number= 0;
+//continoue;
+//} 
+
+//}
+//console.log("the sum is" + sum);
+//print sum of negative numbers
+
+
+var number=0;
+while(number<=0){
+    sum=number;
+
+    number=parseInt(prompt("enter any number")); 
+    if(isNaN(number)){
+        alert("you have entered a string.please enter a number");
+        number= 0;
+        continoue;
+    } 
+
+}
+console.log("the sum is" + sum);
